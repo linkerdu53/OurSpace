5ab30f727cd5feba366c338602f3220a7d4feb91
Linker <q.wascheul@gmail.com>
2021-12-18 15:56:11 +0100

    modification du niveau de son + ajout déplacement des vaisseaux aléatoire dans le fond
    -modification du niveau de son 
    -ajout déplacement des vaisseaux aléatoire dans le fond

--- Assets/Menu/Script/TranslateBackShip.cs
+++ Assets/Menu/Script/TranslateBackShip.cs
@@ -6,22 +6,38 @@ using UnityEngine.UI;
 
 public class TranslateBackShip : MonoBehaviour
 {
+    public GameObject vaisseauBack;
     public float transVitesse;
-    public float positionX;
-    public float positionY;
-    public float positionZ;
+    public float positionXInit;
+    public float positionYInit;
+    public float positionZInit;
+    public float facteurAlea;
+    public int tempsAvRetour;
+    public int tempsAvRetourActuel;
     // Start is called before the first frame update
     void Start()
     {
         //transform.Posi(Vector3(positionX, positionY, positionZ));
-        transform.position = transform.position + new Vector3(positionX, positionY, positionZ);
+        //vaisseauBack = this.GameObject;
+        facteurAlea = Random.Range(1, 5);
+        transform.position = transform.position + new Vector3(positionXInit * facteurAlea, positionYInit* facteurAlea, positionZInit* facteurAlea);
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * transVitesse);
+
+        tempsAvRetourActuel = tempsAvRetourActuel + 1;
+        transform.Translate(Vector3.forward * Time.deltaTime * transVitesse *facteurAlea);
+        if(tempsAvRetourActuel >= tempsAvRetour)
+        {
+            facteurAlea = Random.Range(1, 5);
+            transform.position = new Vector3(positionXInit * facteurAlea, positionYInit * facteurAlea, positionZInit * facteurAlea);
+            tempsAvRetourActuel = 0;
+            
+        }
         //positionX = positionX + transVitesse;
     }
 }
