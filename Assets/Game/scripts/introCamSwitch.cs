fa1a2cc34454c9c7c60d77af02b32da9e76628e6
Linker <q.wascheul@gmail.com>
2021-12-20 15:55:29 +0100

    Modif lvl 2 pour le scrip qu'il se relance a chaque début de relancement de niveau

--- Assets/Game/scripts/introCamSwitch.cs
+++ Assets/Game/scripts/introCamSwitch.cs
@@ -8,15 +8,19 @@ public class introCamSwitch : MonoBehaviour
 {
     public GameObject FirstCamera;
     public GameObject SecondCamera;
+    public int timerActeulAvFinAnimation;
+    public int timeFinAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
-        FirstCamera.SetActive(true);
-        SecondCamera.SetActive(false);
 
-        FirstCamera.SetActive(false);
-        SecondCamera.SetActive(true);
+
+        //FirstCamera.SetActive(true);
+        //SecondCamera.SetActive(false);
+
+        //FirstCamera.SetActive(false);
+        //SecondCamera.SetActive(true);
 
 
     }
@@ -24,7 +28,18 @@ public class introCamSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (timerActeulAvFinAnimation == timeFinAnimation)
+        {
+            FirstCamera.SetActive(false);
+            SecondCamera.SetActive(true);
+            timerActeulAvFinAnimation = timerActeulAvFinAnimation + 1;
+        }
+        else if (timerActeulAvFinAnimation < timeFinAnimation)
+        {
+            FirstCamera.SetActive(true);
+            SecondCamera.SetActive(false);
+            timerActeulAvFinAnimation = timerActeulAvFinAnimation + 1;
+        }
 
     }
 
